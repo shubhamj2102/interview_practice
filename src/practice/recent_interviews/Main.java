08763112bb8d602d08e45968dd227e9feb20b93e
shubhamj2102 <shubhamj2102@gmail.com>
2025-06-03 21:28:17 +0530

    changed something

--- src/practice/recent_interviews/Main.java
+++ src/practice/recent_interviews/Main.java
@@ -3,6 +3,7 @@ package practice.recent_interviews;
 public class Main {
     public static void main(String[] args) {
         System.out.println("This has been an interview");
+        System.out.println("changing something");
     }
 }
 

881d471803591b7893ef5f20d65d3d8fc29f6550
shubhamj2102 <shubhamj2102@gmail.com>
2025-06-03 21:19:14 +0530

    main files added

---
+++ src/interview/main.java
@@ -0,0 +1,8 @@
+package interview;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Hello interviewer");
+        System.out.println("testing");
+    }
+}
